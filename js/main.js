629a28b037418af3dda8118685b019b3d2c84456
Ye1 <K.Ye1@lse.ac.uk>
2025-07-13 20:18:24 +0100

    smooth

--- js/main.js
+++ js/main.js
@@ -11,19 +11,29 @@ function toggleMobileMenu() {
 
 $(document).ready(function () {
   $('a.panel-button').click(function (e) {
+    if ($('.content-wrapper').hasClass('showing')) {
       var clickedHash = $(this).attr('href').split('#')[1];
       var currentHash = window.location.hash.substring(1);
-    if (clickedHash === currentHash) {
-      $('.content-wrapper').removeClass('animated slideInRight')
-      $('.panel-cover').removeClass('panel-cover--collapsed')
-      $('.panel-cover').css('max-width', '100%')
-      $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
-      $('.content-wrapper').removeClass('showing')
-      history.pushState("", document.title, window.location.pathname + window.location.search);
-      //window.location.hash = '' // leaves #
-      e.preventDefault();
-      return;
+      if (clickedHash === currentHash) {
+        $('.content-wrapper').removeClass('animated slideInRight')
+        $('.panel-cover').removeClass('panel-cover--collapsed')
+        $('.panel-cover').css('max-width', '100%')
+        $('.panel-cover').animate({'width': '100%'}, 400, swing = 'swing', function () {})
+        $('.content-wrapper').removeClass('showing')
+        history.pushState("", document.title, window.location.pathname + window.location.search);
+        //window.location.hash = '' // leaves #
+        e.preventDefault();
+        return;
+      }
+      var targetElement = $('#' + clickedHash); // 根据锚点查找目标元素
+      if (targetElement.length) {  // 如果目标元素存在
+        $('html, body').animate({
+          scrollTop: targetElement.offset().top // 滚动到目标元素的位置
+        }, 400); // 400ms的滚动动画
+      }
+
     }
+
     $('.panel-cover').addClass('panel-cover--collapsed');
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
