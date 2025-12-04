1474b115edadf5fa0e857558d29762fc3773c735
noncollapse <122892417+noncollapse@users.noreply.github.com>
2025-12-04 00:33:56 +0000

    update: 更新网站内容

--- home_page/App.tsx
+++ home_page/App.tsx
@@ -64,7 +64,8 @@ const App: React.FC = () => {
                 type: 'system',
                 content: (
                   <div className="mt-4 text-pink-400">
-                    Type <span className="text-white font-bold">'enter'</span> to view commands, or <span className="text-white font-bold">gui</span> to enter legacy Graphical User Interface.
+                    Type <span className="text-white font-bold">'enter'</span> to use the recommended command interface, or <span className="text-white font-bold">'gui'</span> to open the legacy graphical interface (may be outdated).
+
                   </div>
                 )
             }]);
